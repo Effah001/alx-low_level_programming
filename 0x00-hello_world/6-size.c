312ef36e98d7f14a75412a977468335f5097820a
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-16 22:57:23 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 
 int main(void) {
-    printf("Size of char: %d bytes\n", sizeof(char));  
-    printf("Size of int: %d bytes\n", sizeof(int));
-    printf("Size of long int: %d bytes\n", sizeof(long int));
-    printf("Size of float: %d bytes\n", sizeof(float));
-    printf("Size of long long int: %d bytes\n", sizeof(long long int));
+    printf("Size of char: %lu bytes\n", sizeof(char));  
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of long int: %lu bytes\n", sizeof(long int));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of long long int: %lu bytes\n", sizeof(long long int));
     return 0;
 }
