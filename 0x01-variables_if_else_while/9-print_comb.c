3a391ff0b3fd69c15df26fb5f4e4e5613db93ca2
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 19:33:20 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,24 @@
-
-
 #include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function generates a lowercase and uppercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main()
+{
+int i, j;
+for(i = 0; i < 10; i++)
+{
+for(j = 0; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
 
-int main() {
-    int i, j;
-
-    for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            putchar(i + '0');
-            putchar(j + '0');
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    return 0;
+return (0);
 }
