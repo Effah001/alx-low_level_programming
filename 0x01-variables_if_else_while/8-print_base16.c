7c9019a14535f7a8f0b1088a492322ae5964411d
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 17:56:12 +0000

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,16 @@
  *
  * Return: Always 0 (success)
  */
-int main() 
+int main(void)
 {
 int i;
-for(i = 0; i < 16; i++)
+for (i = 0; i < 16; i++)
 {
-if(i < 10)
+if (i < 10)
 {
 putchar(i + '0');
 }
-else 
+else
 {
 putchar(i - 10 + 'a');
 }
@@ -24,5 +24,5 @@ putchar(i - 10 + 'a');
 
 putchar('\n');
 
-return 0;
+return (0);
 }
