474835a82d6b5b5ffc744f16c6f8be3d69a43ca5
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 16:31:30 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  *
  * Return: Always 0 (success)
  */
-int main(void) 
+int main(void)
 {
 char ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-    if (ch != 'e' && ch != 'o')
+if (ch != 'e' && ch != 'o')
 {
-    putchar(ch);
+putchar(ch);
 }
 putchar("\n")
 }
