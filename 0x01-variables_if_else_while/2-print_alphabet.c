6452d6fc228ab4034ae9b276400cf2fe149a4084
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 15:23:49 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function generates a lowercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void) {
     char ch;
-
-    for (ch = 'a'; ch <= 'z'; ch++) {
-        putchar(ch);
+for (ch = 'a'; ch <= 'z'; ch++) {
+    putchar(ch);
     }
 
     return 0;
