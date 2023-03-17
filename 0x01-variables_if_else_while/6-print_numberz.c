9cd21c423607dcf325706312d396b19739088486
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 17:09:12 +0000

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function generates lowercase alphabets, excluding 'e' and 'o'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(char);
+}
+putchar('\n');
+return 0;
+}
