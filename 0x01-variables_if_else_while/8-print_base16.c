570fcec90b34a26b6ad7d7cb18fe9fa8ee5826c3
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 17:34:59 +0000

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function generates a lowercase alphabets in reverse
+ *
+ * Return: Always 0 (success)
+ */
 
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
