02e2db77a2e79ba8edf2f8854ce5c00b5310984b
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 16:55:49 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function generates a lowercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+printf("%c",a);
+}
+putchar('\n');
+return (0);
+}
