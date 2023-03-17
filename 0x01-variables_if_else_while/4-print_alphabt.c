d136d8c869b00797b69bfe52a9f0bee45f9f68ff
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 16:25:58 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function generates a lowercase alphabets
+ *
+ * Return: Always 0 (success)
+ */
 int main(void) {
     char ch;
 
