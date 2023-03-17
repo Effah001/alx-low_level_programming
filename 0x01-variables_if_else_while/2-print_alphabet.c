c677a2862b2642ed14d1b384349ee739394b6870
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-17 15:28:48 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
  * Return: Always 0 (success)
  */
 
-int main(void) {
-    char ch;
-for (ch = 'a'; ch <= 'z'; ch++) {
+int main(void) 
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
     putchar(ch);
-    }
+}
 
     return 0;
 }
