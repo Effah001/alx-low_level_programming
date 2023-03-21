277384c43fed93210382f60bfeec981b368dea6a
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-21 23:40:34 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,25 +5,44 @@
  *
  * Return: Always 0 (Success)
  */
-	void print_to_98(int n)
-	{
-	int i;
 
-	if (n <= 98)
-	{
-		for (i = n; i < 98; i++)
-	{
-		_putchar(i);
-	}
-	}
-	else
-	{
-		for (i = n; i > 98; i--)
-	{
-		_putchar(i);
-	}
-	}
+void print_to_98(int n)
+{
+    int i;
 
-	_putchar("98\n");
+    if (n <= 98) {
+        for (i = n; i <= 98; i++) {
+            int num = i;
+            if (num < 0) {
+                num = -num;
+                putchar('-');
+            }
+            if (num >= 10) {
+                putchar(num / 10 + '0');
+            }
+            putchar(num % 10 + '0');
+            if (i < 98) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    } else {
+        for (i = n; i >= 98; i--) {
+            int num = i;
+            if (num < 0) {
+                num = -num;
+                putchar('-');
+            }
+            if (num >= 10) {
+                putchar(num / 10 + '0');
+            }
+            putchar(num % 10 + '0');
+            if (i > 98) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
+    putchar('\n');
 }
