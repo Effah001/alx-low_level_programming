7966051a5d80592d80481315608c52c6b014cfa1
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-21 20:58:13 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,14 +8,14 @@
 
 int _islower(int c)
 	{
-	char ch = 'c';
 
-	if (_islower(ch))
+	if ( c >= a && <= z)
 	{
-	_putchar('1');
+		_putchar('1');
 	}
 	else
 	{
-	_putchar('0');
+		_putchar('0');
 	}
+		_putchar('\n');
 }
