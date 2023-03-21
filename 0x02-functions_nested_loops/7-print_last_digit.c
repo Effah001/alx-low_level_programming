2983233093a32301fdea76ddebaacd1bf4ea3698
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-21 22:36:13 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,11 @@ int print_last_digit(int num)
 	if (last_digit > 0) 
 	{	
 		_putchar(last_digit + '0');
+		return (last_digit);
 	}
 	else
 	{
-		_putchar(-last_digit + '0')
+		_putchar(-last_digit + '0');
+		return (last_digit);
 	}
 }
