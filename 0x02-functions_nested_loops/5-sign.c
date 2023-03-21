a8a744623225a19b0eff0108332427e47efcbb36
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-21 21:32:04 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int print_sign(int n)
 	{
 	int n = 32;
 	if (n > 0)
