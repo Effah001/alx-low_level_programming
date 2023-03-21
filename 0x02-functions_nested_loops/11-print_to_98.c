dcbc830a259f29d8221da56794b7b55309841f08
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-21 23:28:53 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-	int main(void)
+	void print_to_98(int n)
 	{
 	int i;
 	int n = 90;
@@ -14,18 +14,17 @@
 	{
 		for (i = n; i < 98; i++)
 	{
-		printf("%d, ", i);
+		_putchar("%d, ", i);
 	}
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
 	{
-		printf("%d, ", i);
+		putchar("%d, ", i);
 	}
 	}
 
 	printf("98\n");
 
-	return (0);
 }
