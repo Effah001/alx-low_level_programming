b251b6c2a14bd4c07faad65957aa66b9348caa4c
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-22 00:04:23 +0000

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
+ * _abs - returns the absolute value of an integer
+ * @a: the int to check
  * Return: the absolute value of int
  */
 
