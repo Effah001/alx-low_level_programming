ef282167801c58e5872275a9c085093c0a49dc17
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-22 00:22:18 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,48 +1,35 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+	 * print_to_98 - prints all natural numbers from n to 98,
+	 * followed by a new line
+	 * @n: print from this number
+	 */
+	void print_to_98(int n)
+	{
+        int i, j;
 
-void print_to_98(int n)
-{
-    int i;
 
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            int num = i;
-            if (num < 0) {
-                num = -num;
-                _putchar('-');
-            }
-            if (num >= 10) {
-                _putchar(num / 10 + '0');
-            }
-            _putchar(num % 10 + '0');
-            if (i < 98) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            int num = i;
-            if (num < 0) {
-                num = -num;
-                _putchar('-');
-            }
-            if (num >= 10) {
-                _putchar(num / 10 + '0');
-            }
-            _putchar(num % 10 + '0');
-            if (i > 98) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
+        if (n <= 98)
+		{
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+			}
+		} else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
+		}
+	}
 
-    _putchar('\n');
-}
+
+
