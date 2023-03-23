52aaf10ebe7f2cfa8befa1a32e6d0d8b075c86b3
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-23 23:48:29 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,13 +10,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	n *= -1;
-	i = n;
-	_puchar('-');
+		n *= -1;
+		i = n;
+		_putchar('-');
 	}
 
 	i /= 10;
 	if (i != 0)
 		print_number(i);
-	_putchar((unsigned int) + n % 10 + '0');
+	_putchar((unsigned int)(n % 10) + '0');
 }
