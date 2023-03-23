5586fd513e8d9ddc98753de062e91a17ac408f73
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-23 20:11:40 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,27 +6,28 @@
  * Return: Always 0.
  */
 
-int main()
+int main(void)
 	{
 	int i;
-	for(i = 1; i <= 100; i++)
-	{
 	
-	if(i%3 == 0 && i%5 == 0)
+	for (i = 1; i <= 100; i++)
 	{
 
-		printf("FizzBuzz");
-	}
-	else if (i%3 == 0)
+	if(i % 3 == 0 && i % 5 == 0)
 	{
-		printf("Fizz");
+
+		printf ("FizzBuzz");
 	}
-	else if(i%5 == 0)
+	else if (i % 3 == 0)
 	{
-		printf("Buzz");
+		printf ("Fizz");
 	}
-	else if(i%3 == 0 && i%5 == 0){
-		printf("FizzBuzz");
+	else if(i % 5 == 0)
+	{
+		printf ("Buzz");
+	}
+	else if(i % 3 == 0 && i % 5 == 0){
+		printf ("FizzBuzz");
 	}
 	else
 	{
@@ -36,5 +37,5 @@ int main()
 
 	}
 
-	return 0;
+	return (0);
 }
