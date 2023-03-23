0265513a9591823b48100017a03e385bef773a87
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-23 22:42:06 +0000

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
 {
 	long int n;
 	long int max;
