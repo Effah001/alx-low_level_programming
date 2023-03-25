de08d174a37177ee7915ac6e4ff7685ae94d9178
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-25 11:30:02 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,26 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
- *0i: function
+ * jack_bauer - print every minute of the day
+ *@hour: print hours
+ *@minute : print minutes
  */
 
-void times_table(void)
+void jack_bauer(void)
 	{
-		int i, j, k;
-
-
-		for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-
-
-				if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
+	int hour, minute;
+    
+	for (hour = 0; hour < 24; hour++)
+	{
+        for (minute = 0; minute < 60; minute++)
+	{
+		putchar((hour / 10) + '0');
+		putchar((hour % 10) + '0');
+		putchar(':');
+		putchar((minute / 10) + '0');
+		putchar((minute % 10) + '0');
+		putchar('\n');
+	}
 	}
 
+}
