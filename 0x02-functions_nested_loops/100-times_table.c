88f82eb861cca983ad62495dd2ee4c8c38de52e0
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-25 15:07:36 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,12 +22,17 @@ void print_times_table(int n)
 	}
 	else if (result < 10)
 		{
-		_putchar(','); _putchar(' '); _putchar(' '); _putchar(' ');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
 		_putchar('0' + result);
 		}
 	else if (result < 100)
 	{
-		_putchar(','); _putchar(' '); _putchar(' ');
+		_putchar(','); 
+		_putchar(' ');
+		_putchar(' ');
 		_putchar('0' + result / 10 % 10);
 		_putchar('0' + result % 10);
 	}
