0e7518fe0e5bd63f703edacb476347cfa69edadf
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-25 11:33:05 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,10 @@
 void jack_bauer(void)
 	{
 	int hour, minute;
-    
+	
 	for (hour = 0; hour < 24; hour++)
 	{
-        for (minute = 0; minute < 60; minute++)
+	for (minute = 0; minute < 60; minute++)
 	{
 		putchar((hour / 10) + '0');
 		putchar((hour % 10) + '0');
