dd242e45f25994f7863c604223db8a75ca85a919
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-25 12:17:45 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,10 +17,10 @@ void times_table(void)
 	{
 		_putchar('0' + result / 10 % 10);
 		_putchar('0' + result % 10);
-	} 
-	else 
+	}
+	else
 	{
-		_putchar('0' + result % 10);
+		_putchar('0' + result);
 		_putchar(' ');
 	}
 	if (j < 9)
@@ -29,6 +29,6 @@ void times_table(void)
 		_putchar(' ');
 	}
 	}
-	}
 		_putchar('\n');
+	}
 	}
