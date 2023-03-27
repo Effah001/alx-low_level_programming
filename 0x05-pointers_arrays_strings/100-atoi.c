71e15371f211b68235afa889cb3798268c04c39c
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 20:08:13 +0000

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string to be converted
+ *
+ * Return: The integer value of the string
+ */
+int _atoi(char *s)
+{
+    int result = 0, sign = 1, i = 0;
+    
+    if (s[0] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[0] == '+')
+    {
+        i++;
+    }
+
+    while (s[i] != '\0')
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+        }
+        else
+        {
+            break;
+        }
+
+        i++;
+    }
+
+    return result * sign;
+}
