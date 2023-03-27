9c791d9002e63b4aef7c90c88bbf658e08f85cd4
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 14:51:59 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-*_puts : prints a string to stdout
+*_puts - prints a string to stdout
 *@n : pointer to an integer to be reset
 * Return: nothing
 */
 
 void _puts(char *str)
 	{
-	while (*s != '\0')
+	while (*str != '\0')
 	{
-	putchar(*s);
-	s++;
+	putchar(*str);
+	str++;
 	}
 	putchar('\n');
 	}
