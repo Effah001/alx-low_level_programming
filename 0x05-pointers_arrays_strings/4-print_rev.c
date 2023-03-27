63312e498e3fe57c880876ca81a91be073669e04
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 15:01:43 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,13 @@
 
 	void print_rev(char *s)
 	{
+	int i;
 	int len = 0;
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	for (int i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
