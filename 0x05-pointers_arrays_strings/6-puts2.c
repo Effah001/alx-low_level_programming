5015f508a87af7060c7f27a0ee5f099752d37fcd
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 17:03:51 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
 * puts2 - prints even charaters
+*@str : pointer to the string
 * Return: nothing
 */
 
 void puts2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 	if (i % 2 == 0)
-	{ 
+	{
 		_putchar(str[i]);
 	}
 	i++;
 	}
-		_putchar('\n'); 
+		_putchar('\n');
 }
