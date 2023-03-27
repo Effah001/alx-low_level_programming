30d488cfe1822f9b95e0616049f9b263efe55420
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 08:51:21 +0000

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * reset_to_98 :  resets the value of an integer pointer to 98
+*@n : pointer to an integer to be reset
 *This function takes a pointer to an integer as a parameter
 *and changes the value of the integer to 98
-*@n : pointer to an integer to be reset
 * Return: nothing
 */
 
