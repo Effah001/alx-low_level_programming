f738b54822d807ca5a8e445530d759172a4715e6
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 08:48:33 +0000

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* reset_to_98 : Take in an address
-*@n : the address variable
+* reset_to_98 :  resets the value of an integer pointer to 98
+*@n : pointer to an integer to be reset
 * Return: nothing
 */
 
