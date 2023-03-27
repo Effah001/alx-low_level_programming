f4e827f4e6e1322c95528ea65a66af3a461457e7
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-27 19:59:36 +0000

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
-*print_array :print the values in an array
-* Return: nothing
+*print_array - print the values in an array
+*@a : the pointer pointing to a
+*@n : the length of the string
+*Return: nothing
 */
 
 
 void print_array(int *a, int n)
 	{
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-	if (i < n-1)
+	if (i < n - 1)
 	{
 		printf(", ");
 	}
