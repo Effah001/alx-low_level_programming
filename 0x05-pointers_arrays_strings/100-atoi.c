dd16c065761ba3feca8cf89ee7a6194dc63616cb
Your Name <effahfrancis210@gmail.com>
2023-03-27 16:11:26 -0700

    hey

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,36 +9,36 @@
  */
 int _atoi(char *s)
 {
-    int result = 0; 
-    int sign = 1, i = 0, has_digits = 0;
-    
-    if (s == NULL)
-    {
-        return 0;
-    }
-    
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            sign *= -1;
-        }
-        else if (s[i] == '+')
-        {
-            sign *= 1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
-            has_digits = 1;
-        }
-        else if (has_digits)
-        {
-            break;
-        }
+	int result = 0; 
+	int sign = 1, i = 0, has_digits = 0;
 
-        i++;
-    }
+	if (s == NULL)
+	{
+	return 0;
+	}
 
-    return result * sign;
+	while (s[i] != '\0')
+	{
+	if (s[i] == '-')
+	{
+		sign *= -1;
+	}
+	else if (s[i] == '+')
+	{
+		sign *= 1;
+	}
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		has_digits = 1;
+	}
+		else if (has_digits)
+	{
+	break;
+	}
+
+		i++;
+	}
+
+	return result * sign;
 }
