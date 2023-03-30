241ff382ace888de442b543d68f40947a7a2963d
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 13:09:14 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
 /**
-* reverse_array - reverse the numbers in an array
-*@a : pointer to a integer
-*@n : number of character in the string
+* replace_with_13th - reverse the numbers in an array
+*@s : pointer to a character
 * Return: nothing
 */
+
+void replace_with_13th(char *s)
+   {
+    int i = 0;
+    char c;
+
+    while (s[i] != '\0') {
+        c = s[i];
+        if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		{
+            c = c + 13;
+		}
+            else if (c > 'z') || (c > 'Z') {
+                c = c - 26; // Wrap around to 'a'
+            }
+            s[i] = c;
+        }
+        i++;
+    }
+}
