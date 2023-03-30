4baf8ca5595cefb43cea9f7fad94d1016ece911a
Your Name <effahfrancis210@gmail.com>
2023-03-30 06:12:58 -0700

    hey

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,23 @@
 */
 
 void replace_with_13th(char *s)
-   {
-    int i = 0;
-    char c;
+	{
+	int i = 0;
+	char c;
 
-    while (s[i] != '\0') {
-        c = s[i];
-        if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-		{
-            c = c + 13;
-		}
-            else if (c > 'z') || (c > 'Z') {
-                c = c - 26; // Wrap around to 'a'
-            }
-            s[i] = c;
-        }
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		c = s[i];
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		c = c + 13;
+	}
+	else if (c > 'z') || (c > 'Z')
+	{
+		c = c - 26; 
+	}
+		s[i] = c;
+	}
+		i++;
+	}
 }
