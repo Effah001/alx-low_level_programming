94994c3924b92c50b295d2b529356303a82ce360
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 09:02:23 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,12 @@
 	int i = 0;
 	int j = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[j] != '\0')
 	{
 	i++;
-	}
-
-	while (s2[j] != '\0')
-	{
 	j++;
-
 	}
+	
 	if(s1[i] == s2[j])
 		return (0);
 	else if (s1[i] > s2[j] )
