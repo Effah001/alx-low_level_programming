3fc9efc5ee6771af08aa5f4aa868bcff0d0127ce
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 06:52:44 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,7 @@
 #include "main.h"
 
 char *_strncat(char *dest, char *src, int n)
-<<<<<<< HEAD
-	{
-=======
         {
->>>>>>> aa4d3a2722bb2766eaa1feecce4489241adef6ca
 
 	int i = 0;
 	int j = 0;
