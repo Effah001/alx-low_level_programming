3a9c9fd9a328688fa8519aa9b9c3facc9b8ff5ca
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 09:15:56 +0000

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
+/**
+* reverse_array - reverse the numbers in an array
+*@a : pointer to a integer
+*@n : number of character in the string
+* Return: nothing
+*/
+
 void reverse_array(int *a, int n)
 {
 
-	int i= 0;
+	int i = 0;
 	int j = n -1;
 	int c;
 
-	while ( i < n / 2)
+	while (i < n / 2)
 	{
-
-       		c = a[i];
+		c = a[i];
 		a[i] = a[j - i];
 		a[j - i] = c;
 
