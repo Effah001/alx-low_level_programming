113f589e44be9bfe8212dde0b0fd99f359dff3fd
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 14:07:21 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,24 +6,26 @@
 * Return: nothing
 */
 
-char *rot13(char *s)
-	{
-	int i = 0;
-	char c;
+char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *rot13_alphabet = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+void rot13(char *s)
+{
+	int i = 0;
 	while (s[i] != '\0')
 	{
-		c = s[i];
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	int j = 0;
+	while (j < 52 && s[i] != alphabet[j])
 	{
-		c = c + 13;
+		j++;
 	}
-	else if ((c > 'z') || (c > 'Z'))
+	if (j < 52)
 	{
-		c = c - 26; 
+		s[i] = rot13_alphabet[j];
 	}
-		s[i] = c;
 		i++;
 	}
+}
+
 return (s);
 }
