e30ac158697b569e944a3d8ef32b54d09ec400b7
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 23:07:47 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,11 @@
 *@s2: pointer to a character
 * Return: nothing
 */
-	
+
 	int _strcmp(char *s1, char *s2)
 	{
 	int i = 0;
+
 	while (s1[i] == s2[i] && s1[i] == '\0')
 	{
 	i++;
@@ -19,7 +20,7 @@
 	return (0);
 	}
 	else if (s1[i] > s2[i])
-	{	
+	{
 		return (1);
 	}
 	else
