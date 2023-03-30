273e79e18f01956f04fc0cc4edb684aa88854c54
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 06:57:40 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* _strncat - concatenate two strings based on n
+*@dest : destination string to be concatenated to
+*@src : source string
+*Return: nothing
+*/
+
 char *_strncat(char *dest, char *src, int n)
 	{
 
