d1015d22417298958c60595d45ef19cf5b87ca1d
Droid <44108804+Effah001@users.noreply.github.com>
2023-03-30 07:11:51 +0000

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,12 +3,13 @@
 * _strncpy - copies a sting into another string
 *@dest : destination string to be copied to
 *@src : source string
+*@n : number of strings to be copied
 *Return: nothing
 */
 
 char *_strncpy(char *dest, char *src, int n)
 	{
-	int i,j = 0;
+	int i , j = 0;
 
 	while (src[j] != '\0' && j < n)
 	{
@@ -16,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 	j++;
 	i++;
 	}
-	dest[i] ='\0';
+	dest[i] = '\0';
 
 	return (dest);
 	}
