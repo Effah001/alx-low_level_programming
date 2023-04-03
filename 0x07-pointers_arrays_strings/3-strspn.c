da47c9b2ad4fabe9dbd648df67f2395c5072b492
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-03 21:51:46 +0000

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,6 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int j = 0;
 	unsigned int count = 0;
+	
 	while (s[i] != '\0')
 	{
 		j = 0;
@@ -31,5 +32,5 @@ unsigned int _strspn(char *s, char *accept)
 		}
 			i++;
 		}
-	return count;
+	return (count);
 }
