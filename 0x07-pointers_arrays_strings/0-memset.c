28d53bcf0895ea0e6e7cdd6462524ab5fc156f37
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-03 21:32:23 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,14 @@
 */
 
 
-char* _memset(char* s, char b, unsigned int n)
+char* _memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	
-	while(i < n)
+
+	while (i < n)
 	{
 		s[i] = b;
 		i++;
 	}
-	return s;
+	return (s);
 }
