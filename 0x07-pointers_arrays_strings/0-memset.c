1c372381085f638d5717e6a3ea1f84308a47d90a
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-03 21:27:58 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+* _memset -  a function that fills memory with a constant byte
+*@s : string to be filled
+*@b : character to be filled inside the memory
+*@n : number of memory space to be filled
+*Return: s
+*/
+
 
 char* _memset(char* s, char b, unsigned int n)
 {
