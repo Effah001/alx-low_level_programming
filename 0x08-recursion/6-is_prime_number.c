ace0acca96d45447b2b3248d2da7818e9a942baf
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-05 19:44:48 +0000

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,16 +2,16 @@
 
 int is_prime_number(int n)
 {
-	int x;
-
 	if (n < 2)
-		return 0;
+	return (0);
+	return (is_prime_helper(n, 2));
+}
 
-	for (x = 2; x < n; x++)
-	{
+int is_prime_helper(int n, int x)
+{
+	if (x >= n)
+		return (1);
 	if (n % x == 0)
-		return 0;
-	}
-
-	return 1;
+		return (0);
+	return (is_prime_helper(n, x + 1));
 }
