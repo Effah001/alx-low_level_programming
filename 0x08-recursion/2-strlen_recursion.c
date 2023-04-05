90fc63aaefb551256529ef3ca3813afcc9eafc6b
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-05 16:43:39 +0000

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+* _strlen_recursion - a function that prints the length of the string.
+*@s : the length of the string to be displayed
+*/
 
 void _strlen_recursion(char *s)
 {
