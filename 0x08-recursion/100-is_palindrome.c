d5919d4054b9c80735319ff95c969419d3f37b4a
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-05 20:10:10 +0000

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,10 @@
 #include "main.h" 
-
+/**
+* is_palindrome -  a function that returns 1 if a string is a palindrome 
+* and 0 if not.
+*@str : input string
+*Return: 1 or 0
+*/
 int is_palindrome(char * str)
 {
 	int x;
