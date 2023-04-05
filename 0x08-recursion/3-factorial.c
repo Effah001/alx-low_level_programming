399d60bec299e0a39f9d93ed6fab291e437a1078
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-05 20:06:21 +0000

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,12 +1,13 @@
 /**
 * factorial - a function that returns the factorial of a given number
 *@n : the number to be factorized.
+*Return: Factorial
 */
 
 int factorial(int n)
 {
 	if (n == 1)
-	{	
+	{
 		return (1);
 	}
 	if (n < 0)
