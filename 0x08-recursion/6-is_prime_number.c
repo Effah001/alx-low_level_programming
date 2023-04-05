3e4a5052b6f80b8026feb0bf80c9c0373ac20ec7
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-05 19:53:01 +0000

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,7 +2,7 @@
 /**
 * is_prime_number - a function that returns 1 if the input integer
 * is a prime number, otherwise return 0
-* is_prime_helper - help is_prime_number
+* is_prime_helper - a function that helps _is_prime_number
 *@n: input number
 *Return: Prime or not
 */
