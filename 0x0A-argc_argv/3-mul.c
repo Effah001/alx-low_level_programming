172394b65e760aed2e00159df8a4b3969ad341c8
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-08 19:38:21 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,24 +2,33 @@
 #include <stdlib.h>
 #include "main.h"
 
-int main(int argc,char * argv[])
+/**
+* main - multiplies numbers
+*@argc : arguments count
+*@argv : argument vector
+* Return: zero
+*/
+
+
+int main(int argc, char *argv[])
 {
 	int i;
 	int mul = 1;
 
 	if (argc != 3)
-	{	
+	{
 		_puts("Error\n");
 		return (1);
 	}
-	
 
-	for(i = 1; i < argc; i++)
+
+	for (i = 1; i < argc; i++)
 	{
 		int m =  atoi(argv[i]);
+		
 		mul = mul * m;
 	}
-		
-		_puts(itoa(null));
+
+		_puts (itoa(null));
 		return(0);
 }
