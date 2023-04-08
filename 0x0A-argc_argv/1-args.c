f8fe07201f2f591a4661e6164c31873ad2fba313
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-08 16:34:58 +0000

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+/**
+* main - prints out it name.
+*@argc : arguments count
+*@argv : argument vector
+* Return: zero
+*/
 
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	_putchar(argc + '0');
+	_putchar('\n');
 	return 0;
 }
