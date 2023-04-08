165a26a7fe75034e881e7f3311c606a03f1b85d7
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-08 16:23:52 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,13 @@
 #include "main.h"
+/**
+* main - prints out it name
+*
+*/
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	char *s = argv[0];
-	
+
 	while (*s != '\0')
 	{
 	_putchar(*s);
