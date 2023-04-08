076663d7cacb46c5228ee799fa632a06e54dc6a6
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-08 19:46:34 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc,char * argv[])
+/**
+* main - add  numbers
+*@argc : arguments count
+*@argv : argument vector
+* Return: zero
+*/
+
+int main(int argc ,char *argv[])
 {
-        int add = 0;
+	int add = 0;
 	int i;
 
 	if (argc == 1)
@@ -12,13 +19,13 @@ int main(int argc,char * argv[])
 		printf("0\n");
 	}
 
-        for(i = 1; i < argc; i++)
-      	{
+	for(i = 1; i < argc; i++)
+	{
 	
 		int a = atoi(argv[i]);
 		add = add + a;
         }
 
-                printf("%d\n", add);
+                printf ("%d\n", add);
                 return(0);
 }
