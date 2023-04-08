d26404b4b28734289e4bae59cda9701d92f70bbb
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-08 17:13:24 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints out it name.
+*@argc : arguments count
+*@argv : argument vector
+* Return: zero
+*/
 
 int main(int argc , char *argv[])
 {
+	char *s = argv[0];
 	int i;
 	for(i = 0; i <argc; i++)
 	{
-	printf("%s\n",argv[i]);
+	while (*s != '\0')
+	_putchar(*s);
+		s++
+	}
+	putchar('/n');
 	}
 		return 0;
 }
