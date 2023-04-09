4a110bd87a4449757283591780cdb34c157f4341
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 12:58:18 +0000

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+ * _abs - returns the absolute value of an integer
+ * @a: the int to check
+ * Return: the absolute value of int
+ */
+
 int _abs(int n)
-{
+	{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
 }
