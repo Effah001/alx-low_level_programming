7b922d6cdf9aeb2c9b95c129d9bedc1cb6058846
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 12:28:57 +0000

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,5 +1,23 @@
+#include <stddef.h>
 #include "main.h"
+/**
+* _strchr - a function that locate a character in a string
+*@s : string to be searched
+*@c : character to be located
+*Return: nothing
+*/
 
 char *_strchr(char *s, char c)
 {
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+	if (s[x] == c)
+	{
+		return (s);
+	}
+		s++;
+	}
+	return (NULL);
 }
