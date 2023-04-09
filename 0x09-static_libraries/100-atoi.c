c8d9423108c28b4a09ab919afe363f17998ced1b
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 12:26:18 +0000

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,42 @@
+#include <stddef.h>
 #include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string to be converted
+ *
+ * Return: num 
+ */
 int _atoi(char *s)
 {
+	int num = 0;
+	int i = 0;
+	int is_neg = 0;
+	int num_of_neg = 0;
+
+	while (s[i] == '-' || s[i] == '+')
+	{
+	if (s[i] == '-')
+	{
+		num_of_neg++;
+        }
+		i++;
+	}
+	
+	if (num_of_neg % 2 == 1)
+	{
+		is_neg = 1;
+	}
+	
+	for (; s[i] != '\0'; i++)
+	{
+		num = num * 10 + (s[i] - '0');
+	}
+	
+	if (is_neg)
+	{
+		num = -num;
+	}
+
+return num;
 }
