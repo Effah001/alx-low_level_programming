5a4fba658f7255025b515d730de081d11fba9753
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 13:45:56 +0000

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,4 +1,21 @@
 #include "main.h"
+/**
+* _strcpy -  copies a string pointed by str to dest
+*@src : pointer for thr initial string
+*@dest : pointer to be copied to
+*Return: nothing
+*/
+
 char *_strcpy(char *dest, char *src)
-{
+	{
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	}
+		*dest = '\0';
+	return (dest_start);
 }
