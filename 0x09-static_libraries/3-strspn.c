ce0fbc866e21c322db519bd3eaf79db90899a3fe
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 12:46:30 +0000

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,4 +1,35 @@
 #include "main.h"
+
+/**
+* _strspn - a function that gets the length of a prefix substring
+*@s : string to be searched
+*@accept : character to be located
+*Return: nothing
+*/
+
 unsigned int _strspn(char *s, char *accept)
 {
+	int x = 0;
+	int y = 0;
+	unsigned int count = 0;
+
+	while (s[x] != '\0')
+	{
+		y = 0;
+		while (accept[y] != '\0')
+		{
+			if (s[x] == accept[y])
+			{
+				count++;
+				break;
+			}
+			y++;
+		}
+		if (accept[y] == '\0')
+		{
+			break;
+		}
+			x++;
+		}
+	return (count);
 }
