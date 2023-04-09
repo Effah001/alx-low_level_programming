de8b5d8eb5519fbb33c12af69c132bef0c36a2b4
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 12:53:40 +0000

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,4 +1,31 @@
 #include "main.h"
+#include <stddef.h>
+
+/**
+* _strstr - a function that locates a substring.
+*@haystack : string to be searched
+*@needle : string to be located
+*Return: nothing
+*/
+
 char *_strstr(char *haystack, char *needle)
 {
+	int x;
+	int y;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+	for (y = 0; needle[y] != '\0'; y++)
+	{
+		if (haystack[x + y] != needle[y])
+		{
+		break;
+	}
+	}
+	if (needle[y] == '\0')
+	{
+		return (&haystack[x]);
+	}
+	}
+	return (NULL);
 }
