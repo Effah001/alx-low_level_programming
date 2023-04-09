60ca0a9d668825f9bdfb41665933767b156d4e02
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-09 13:44:54 +0000

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,7 +1,17 @@
 #include "main.h"
 /**
-*_strlen - find the length
+*_strlen - calculate the lenght of a string
+*@s : pointer to the string
+* Return: length
 */
-int _strlen(char *s)
-{
-}
+
+	int _strlen(char *s)
+	{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+	len++;
+	}
+	return (len);
+	}
