fff8de511eb3cd313ced38010627bafb9aa3c851
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-14 10:37:38 +0000

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -48,6 +48,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 	dest[i] = src[i];
 	}
+	return (new_ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	src = ptr;
+	dest = new_ptr;
+	for (i = 0; i < old_size; i++)
+	{
+	dest[i] = src[i];
+	}
 	free(ptr);
 
 	return (new_ptr);
