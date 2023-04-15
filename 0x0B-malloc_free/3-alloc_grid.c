65c25f2552fab1726717c6c479e065a591e4686e
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-15 20:18:35 +0000

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = (int **) malloc(sizeof(int *) * height);
+	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 	{
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-	grid[i] = (int *)malloc(sizeof(int) * width);
+	grid[i] = malloc(sizeof(int) * width);
 	if (grid[i] == NULL)
 	{
 		for (j = 0; j < i; j++)
