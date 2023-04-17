7e0a256a41fdfc8fad76a6c6895d6be2cd9c1871
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-17 20:14:05 +0000

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,14 +16,18 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = malloc(_strlen(name) + 1);
 	if (d->name != NULL)
+	{
 	_strcpy(d->name, name);
-
+	}
+	
 	d->age = age;
 
 	d->owner = malloc(_strlen(owner) + 1);
 	if (d->owner != NULL)
+	{
 	_strcpy(d->owner, owner);
-
+	}
+	
 	if (d->name == NULL || d->owner == NULL)
 	{
 	free(d->name);
