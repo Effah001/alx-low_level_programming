39ee89add172760f868427aac5e0c885833dde56
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-17 14:02:56 +0000

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+*free_dog -  free dog
+*@name : name of dog
+*@owner : owner of dog
+*@age : age of dog
+*@d : pointer to struct dog
+*Return: nothing
+*/
+
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
