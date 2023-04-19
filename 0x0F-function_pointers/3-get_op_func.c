8382494319467cf05a9e9ca7dd5b743f5e9049a3
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-19 20:43:46 +0000

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,11 +4,15 @@
 #include "function_pointers.h"
 #include "3-calc.h"
 
+/**
+*get_op_func - get the operation
+*/
+
 int (*get_op_func(char *s))(int, int)
 {
 
 
-	int i = !strcmp(s, "+") * 1 +!strcmp(s, "-") * 2 + !strcmp(s, "*") * 3 + !strcmp(s, "/") * 4 + !strcmp(s, "%") * 5;
+	int i = !strcmp(s, " + ") * 1 +!strcmp(s, "-") * 2 + !strcmp(s, "*") * 3 + !strcmp(s, "/") * 4 + !strcmp(s, "%") * 5;
 	int (*op_funcs[6])(int, int) = {op_add, op_sub, op_mul,  op_div, op_mod};
-	return i ? op_funcs[i - 1] : NULL;
+	return (i ? op_funcs[i - 1] : NULL);
 }
