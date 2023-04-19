e7a33abee3905388cba8c9913c19701774cf5198
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-19 20:45:25 +0000

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,13 +6,16 @@
 
 /**
 *get_op_func - get the operation
+*@s : string
+*@op_funcs : funtion pointer
 */
 
 int (*get_op_func(char *s))(int, int)
 {
 
 
-	int i = !strcmp(s, " + ") * 1 +!strcmp(s, "-") * 2 + !strcmp(s, "*") * 3 + !strcmp(s, "/") * 4 + !strcmp(s, "%") * 5;
+	int i = !strcmp(s, " + ") * 1 +!strcmp(s, "-") * 2 + !strcmp(s, "*") * 3 +
+	!strcmp(s, "/") * 4 + !strcmp(s, "%") * 5;
 	int (*op_funcs[6])(int, int) = {op_add, op_sub, op_mul,  op_div, op_mod};
 	return (i ? op_funcs[i - 1] : NULL);
 }
