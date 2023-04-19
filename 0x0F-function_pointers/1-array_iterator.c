496466bbfe6abc5c4b3ddc9bc1c40d5143ade7b2
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-19 19:53:07 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+**array_iterator - iterate through an array
+*array : hold the array of integers
+*action : pointer to function
+*/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
