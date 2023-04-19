f0b5685bd3cdfe09c837e9732a1ae58cb13285e6
Your Name <effahfrancis210@gmail.com>
2023-04-19 12:32:31 -0700

    Hey

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "function_pointers.h"
 #include "3-calc.h"
 
@@ -6,35 +8,7 @@ int (*get_op_func(char *s))(int, int)
 {
 
 
-	int i = !_strcmp(s, "+") * 1 +!_strcmp(s, "-") * 2 + !_strcmp(s, "*") * 3 + !_strcmp(s, "/") * 4 + !_strcmp(s, "%") * 5;
-	int (*op_funcs[5])(int, int) = {op_add, op_sub, op_mul,  op_div, op_mod};
-	return i ? op_funcs[i] : NULL;
+	int i = !strcmp(s, "+") * 1 +!strcmp(s, "-") * 2 + !strcmp(s, "*") * 3 + !strcmp(s, "/") * 4 + !strcmp(s, "%") * 5;
+	int (*op_funcs[6])(int, int) = {op_add, op_sub, op_mul,  op_div, op_mod};
+	return i ? op_funcs[i - 1] : NULL;
 }
-
-/**
-* _strcmp - compare to strings
-*@s1: pointer to a character
-*@s2: pointer to a character
-* Return: nothing
-*/
-
-	int _strcmp(char *s1, char *s2)
-	{
-	int i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-	i++;
-	}
-	if (s1[i] == s2[i])
-	{
-	return (0);
-	}
-	else if (s1[i] > s2[i])
-	{
-		return (1);
-	}
-	else
-		return (-1);
-}
-
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,7 +8,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+
 
 void print_name(char *name, void (*f)(char *));
 int _strcmp(char *s1, char *s2);
