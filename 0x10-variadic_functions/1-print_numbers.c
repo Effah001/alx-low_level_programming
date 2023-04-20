00869f12b66378067d34c9924415150ff5a4a3f9
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-20 16:58:41 +0000

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,12 +2,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+/**
+*print_numbers - sum all the characters
+*@n : number of arguments
+*...: Variable number of parameters
+*Return: none
+*/
+
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
         va_list print;
         unsigned int i;
 
-        if( separator == NULL)
+        if (separator == NULL)
                 return;
 
         va_start(print, n);
@@ -16,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
         {
             printf("%d%s" ,va_arg(print, int), separator);
         }
-            printf("%d\n", va_arg(print,int));
+            printf("%d", va_arg(print,int));
         
         va_end(print);
 }
