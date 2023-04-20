3864d307548fa793f053bfd8718acf1f12925158
Your Name <effahfrancis210@gmail.com>
2023-04-19 20:06:50 -0700

    me

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,34 +10,34 @@
 
 int main(int argc, char *argv[])
 {
-    char *address;
-    int nbytes;
-    int i;
+	char *address;
+	int nbytes;
+	int i;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    nbytes = atoi(argv[1]);
+	nbytes = atoi(argv[1]);
 
-    if (nbytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
-    address = (char *)main;
+	address = (char *)main;
 
-    for (i = 0; i < nbytes; i++)
-    {
-        printf("%02x", address[i] & 0xff);
-        if (i < nbytes - 1)
-            printf(" ");
-    }
+	for (i = 0; i < nbytes; i++)
+	{
+	printf("%02x", address[i] & 0xff);
+	if (i < nbytes - 1)
+		printf(" ");
+	}
 
-    printf("\n");
+		printf("\n");
 
-    return (0);
-}
+		return (0);
+	}
