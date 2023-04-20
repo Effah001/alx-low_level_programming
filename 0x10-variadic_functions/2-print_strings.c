9f073b48cba0d22509d720dab18981733104735f
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-20 17:53:26 +0000

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,9 +8,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *s;
 
-	if(separator == NULL)	
-	return;
-
 	va_start(print, n);
 
 	for (i = 0; i < n ; i++)
