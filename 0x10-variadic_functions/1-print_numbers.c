c3872adc1593121ac578adcd76912b7d32912e37
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-20 17:50:18 +0000

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,6 +6,7 @@
 /**
 *print_numbers - sum all the characters
 *@n : number of arguments
+*@separator : separation
 *...: Variable number of parameters
 *Return: none
 */
@@ -15,14 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list print;
 	unsigned int i;
-
-<<<<<<< HEAD
-	if (separator == NULL)
-	return;
-=======
-        
->>>>>>> fd303633a17c88ea848d70b5bcb0ba3c4a35977e
-
+	
 	va_start(print, n);
 
 	for (i = 0; i < n ; i++)
