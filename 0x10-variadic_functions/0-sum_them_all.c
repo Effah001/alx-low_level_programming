76ddbaa093a20774c3d6a1032c6a80d0303d8781
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-20 16:40:28 +0000

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
+/**
+*sum_them_all - sum all the characters
+*Return: sum
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list add;
@@ -8,19 +13,19 @@ int sum_them_all(const unsigned int n, ...)
 	int x;
 	int sum = 0;
 
-	if( n == 0)
-		return(0);
+	if (n == 0)
+		return (0);
 
 
 	va_start(add, n);
 
 	for (i = 0; i < n ; i++)
 	{
-		x = va_arg(add , int);
+		x = va_arg(add, int);
 		sum += x;
 	}
 
 	va_end(add);
 
-	return sum;
+	return (sum);
 }
