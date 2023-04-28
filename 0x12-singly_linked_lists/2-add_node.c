f4dd7eebb26aaccab30b51faaa748e7296db2aa7
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-28 04:03:00 +0000

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,12 +22,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 
-	while(*str != '\0')	
+	while (*str != '\0')
 	{
 	str++;
 	x++;
 	}
-	
+
 	new_node->len = x;
 	new_node->next = *head;
 	*head = new_node;
