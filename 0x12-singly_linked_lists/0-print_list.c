a4772ee58052a331ec7b5902379a3852b971ca7a
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-28 05:28:58 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,9 +22,9 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", 0, "(nill)");
 
 	else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
+		printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	count++;
 	}
 	return (count);
 	}
