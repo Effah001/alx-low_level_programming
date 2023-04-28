ccec503f96eab2d4f0b8fe3a25aa46bb0b14d97a
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-28 03:57:27 +0000

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,19 +7,26 @@
 * add_node - add a node to the beginning
 * @str: pointer to the string
 *@head: pointer to the first element
-*Return: count
+*Return: address of the new node
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int x = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
+
 	new_node->str = strdup(str);
 
+	while(*str != '\0')	
+	{
+	new_node->len = x;
+	x++;
+	}
 	new_node->next = *head;
 	*head = new_node;
 
