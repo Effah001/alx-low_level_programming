c33f63ac30f4ae4227a9a0db95ff6eeb0dd9cb2d
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-30 08:47:05 +0000

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ *free_listint2 - print all elements
+ *@head: pointer to the first node
+ *
+ *Return: count
+ */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
