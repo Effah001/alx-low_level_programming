095d5c54847ad37f218b8e83cf7f597fd0b10dd3
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-30 08:44:06 +0000

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,10 +3,17 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ *add_nodeint_end - print all elements
+ *@head: pointer to the first node
+ *@n: integer
+ *Return: count
+ */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
-	
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
