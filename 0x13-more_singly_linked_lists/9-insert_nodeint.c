60f990d6649cf1cc5a3d85c9e529d113cf3b897b
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-01 09:25:16 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *temp = *head;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 	return (NULL);
 
 	for (i = 0; i < idx; i++)
