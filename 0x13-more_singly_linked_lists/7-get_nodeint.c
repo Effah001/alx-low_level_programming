bc2391a885f541854193c785c8b885eab82a3175
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-01 08:56:01 +0000

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,5 +19,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 	}
 
-	return (i < index && head != NULL) ? (head) : (NULL);
+	return (i == index && head != NULL) ? (head) : (NULL);
 }
