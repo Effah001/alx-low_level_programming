f96b220a0b2f8f2d23e8e666d787900166c20741
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-01 08:37:11 +0000

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,19 +4,20 @@
 #include "lists.h"
 
 /**
-*get_nodeint_at_index - a function that deletes the head node
+*get_nodeint_at_index - a function that get a node at an index
 *@head: pointer to the list
+*@index: index to be searched
 *Return: integer
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
-	for(i = 0; i < index; i++)
+
+	for (i = 0; i < index; i++)
 	{
 		head = head->next;
 	}
-	
-	return (i == index) ? head : NULL;
+
+	return (i == index) ? (head : NULL);
 }
