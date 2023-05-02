95b09ae1f713de774c72a1aa70e84b37ab2ed4c2
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-02 16:40:53 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,17 +13,17 @@ size_t print_listint_safe(const listint_t *head)
 {
 	int i = 0;
 	listint_t *temp = head;
-	int visit[1000] = {0};
+	int check[1000] = {0};
 
 	while (temp)
 	{
-		if (visited[(size_t)temp % 10000])
+		if (check[(size_t)temp % 10000])
 		{
 			printf("-> [%p] %d\n", (void *)temp, temp->n);
 			break;
 		}
 		
-		(visit[(size_t)temp % 1000]) = 1;
+		(check[(size_t)temp % 1000]) = 1;
 		printf("[%p] %d\n", (void *)temp, temp->n);
 
 		temp = temp->next;
