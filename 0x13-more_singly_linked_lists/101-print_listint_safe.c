c1b0539cb2741899bae5e94eac45fa88195eac51
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-02 13:52:12 +0000

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,43 +11,34 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *prev = head;
-	const listint_t *forw = head->next;
-
 	int i = 0;
-	int condition = head < head->next ? 1 : 0;
-
-	if (head != NULL)
-		printf("[%p] %d\n", (void *)prev, prev->n);
-
-	while (prev != NULL)
+	const listint_t *prev = head;
+	const listint_t *forw = head;
+	
+	while (forw && forw->next)
 	{
-		if (condition == 1)
-		{
-			if (forw < prev)
-			{
-				printf("[%p] %d\n", (void *)forw, forw->n);
-				exit(98);
-			}
-		}
-
-		else
-		{
-			if (forw > prev)
-			{
-				printf("[%p] %d\n", (void *)forw, forw->n);
-				exit(98);
-			}
-		}
-
+		prev = prev->next;
+		forw = forw->next->next;
 		i++;
-		printf("[%p] %d\n", (void *)forw, forw->n);
-		prev = forw;
-		if (prev->next == NULL)
+
+		if (prev == forw)
 		{
-		break;
+			printf("-> [%p] %d\n", (void *)prev, prev->n);
+			i++;
+			break;
 		}
-			forw = forw->next;
+		
+		print("[%p] %d\n", (void *)prev, prev->n);
+	}
+	
+	if (!forw || !forw->next)
+	{
+		while (slow)
+		{
+			print("[%p] %d\n", (void *)prev, prev->n);
+			prev = prev->next;
+			i++;
 		}
-			return (i);
+	}
+	return (i);
 }
