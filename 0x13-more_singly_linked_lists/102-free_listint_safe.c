a67a074a8abe7518a396d889d751483ecc7cb1cc
Your Name <effahfrancis210@gmail.com>
2023-05-02 10:31:39 -0700

    hey

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *print_listint_safe - prints a safe list
+ *@head: pointer to the first node
+ *Return: address of new node
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	int i = 0;
+	listint_t *temp = *h;
+	int check[20000] = {0};
+	listint_t *next;
+	
+	while (temp)
+	{
+		if (check[(size_t)temp % 20000])
+		{
+			*h = NULL;
+			return (i);
+		}
+		
+		(check[(size_t)temp % 20000]) = 1;
+		
+		next = temp->next;
+		free(temp);
+		temp = next;
+
+		i++;
+	}
+	*h = NULL;
+	return (i);
+}
