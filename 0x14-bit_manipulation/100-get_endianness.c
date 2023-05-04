cbbe50654e51c2dbe1543848c6eaccd3668b8904
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-04 14:23:24 +0000

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+*get_endianness - a function that checks the endianness
+*Return: endianness
+*/
+
 int get_endianness(void)
 {
 	unsigned int a = 1;
-	char *b = (char*)&a;
+	char *b = (char * )&a;
 
-	return(*b);
+	return (*b);
 }
