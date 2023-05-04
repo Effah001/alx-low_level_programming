b3dcc6414c70e42f44cb8cfbe8925add2bca17f7
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-04 13:57:23 +0000

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ *get_bit - a function that returns the value of a bit at a given index.
+ *@n : the value of bit
+ *@index : index to found
+ *Return: the value of bit at the index
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > (sizeof(unsigned long int) * 8 -1))
