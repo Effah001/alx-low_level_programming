8dccd37a8ae4f09deefcb55c060e94bd061362ac
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-04 14:04:29 +0000

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ *set_bit - a function that sets the value of a bit to 1 at a given index.
+ *@n : the value of bit
+ *@index : index to found
+ *Return: the value of bit at the index
+ */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8 - 1)
