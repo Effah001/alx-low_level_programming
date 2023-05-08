ad4dc9dd58088b51a0b67624a4953972bd11b733
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-08 23:33:50 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,18 +41,18 @@ char buffer[1024];
 size_t n_read, n_write;
 
 f1 = fopen(file_from, "r");
-if (!f1)
-{
-	fprintf(stderr, "Error: Can't read from file NAME_OF_THE_FILE %s\n", file_from);
-	exit(98);
-}
+	if (!f1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 
 f2 = fopen(file_to, "w");
-if (!f2)
-{
-	fprintf(stderr, "Error: Can't write from file NAME_OF_THE_FILE %s\n", file_to);
-	exit(99);
-}
+	if (!f2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
+		exit(99);
+	}
 
 chmod(file_to, 0664);
 
@@ -61,20 +61,20 @@ while ((n_read = fread(buffer, 1, sizeof(buffer), f1)) > 0)
 	n_write = fwrite(buffer, 1, n_read, f2);
 	if (n_write != n_read)
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
 		exit(99);
 	}
 }
 
 if (fclose(f1) == EOF)
 	{
-	printf("Error: Can't close fd FD_VALUE %d\n", fileno(f1));
+	dprintf(STDERR_FILENO, "Error: Can't close file %s\n", file_from);
 	exit(100);
 	}
 
 if (fclose(f2) == EOF)
 	{
-	printf("Error: Can't close fd FD_VALUE %d\n", fileno(f1));
+	dprintf(STDERR_FILENO, "Error: Can't close file %s\n", file_from);
 	exit(100);
 	}
 }
