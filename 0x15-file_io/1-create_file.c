afd91f291976c63236556bc3fddc9902eddb9607
Droid <44108804+Effah001@users.noreply.github.com>
2023-05-08 21:42:37 +0000

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,13 @@
 #include <sys/stat.h>
 #include "main.h"
 
+/**
+ *create_file - a function that creates a file
+ *@filename : pointer to the file
+ *@text_content : content to be written 
+ *Return: the number of characters
+ */ 
+
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp = fopen(filename, "w");
@@ -13,9 +20,9 @@ int create_file(const char *filename, char *text_content)
 
 	chmod(filename, S_IRUSR | S_IWUSR);
 
-	if(text_content)
+	if (text_content)
 	{
-		fprintf(fp,"%s", text_content);
+		fprintf(fp, "%s", text_content);
 	}
 
 	fclose(fp);
