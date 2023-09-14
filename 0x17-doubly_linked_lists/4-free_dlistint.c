caeb2888708899ef4c97cb0a104fb30873dfa600
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 16:22:19 +0000

    Create 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+  /**
+ *free_listint2 - free the list
+ *@head: pointer to the list
+ *
+ *Return: none
+ */
+ 
+void free_dlistint(dlistint_t *head){ 
+	listint_t *temp;
+
+	if (head == NULL)
+	return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+		*head = NULL;
+}
