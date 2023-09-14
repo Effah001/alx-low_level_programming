022ca91cc4688f228edde79111dd1e2685a0b36e
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 22:36:05 +0000

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,14 @@
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h){
+size_t print_dlistint(const dlistint_t *h)
+{
     const dlistint_t *current = h;
 
     size_t count = 0;
 
-    while (current != NULL){
+    while (current != NULL)
+    {
         printf("%d\n", current->n);
         current = current->next;
         count++;
