b1703a29684bc07cd0ef3a3972e06c134e6fafc1
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 13:22:31 +0000

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,21 @@
+#include "lists.h"
 
+/**
+ * dlistint_len - prints the number of elements
+ * of a list
+ *
+ * @h: points to head of linked list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h){
+    const dlistint_t *current = h;
+
+    size_t count = 0;
+
+    while (current != NULL) {
+        current = current->next;
+        count++;
+    }
+    return count;
+}
