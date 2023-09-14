5114031f1b9ce0c002b4e6987a7b1b9c77a25b31
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 10:47:20 +0000

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,24 @@
+#include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: points to head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h){
+    const dlistint_t *current = h;
+
+    size_t node_count = 0;
+
+    while (current != NULL) {
+        printf("%d ", current->n);
+        current = current->next;
+        node_count++;
+    }
+    printf("\n");
+
+    return node_count;
+}
