256662bbcc2c59bd4845660757a2bcf56f9bf3cd
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 14:37:55 +0000

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,12 +18,17 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n){
 
     new_node->n = n;
     new_node->next = NULL;
+
+    if (*head == NULL) {
+        new_node->prev = NULL;
+        *head = new_node;
+    } else {
     
     while (current->next != NULL) {
             current = current->next;
     }
         current->next = new_node;
         new_node->prev = current;
- 
+    }
     return (new_node);
 }
