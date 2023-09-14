bc1b70f51fa5a9d5e120592a2e300aab915020be
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 16:29:17 +0000

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -15,9 +15,9 @@ void free_dlistint(dlistint_t *head){
 
 	while (head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = head;
+		head = (head)->next;
 		free(temp);
 	}
-		*head = NULL;
+		head = NULL;
 }
