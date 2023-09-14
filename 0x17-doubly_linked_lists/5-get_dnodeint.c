f9a6b6bee3e827aad974e1a4d260d4a6814ca988
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 16:55:44 +0000

    Create 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+/**
+ * get_dnodeint_at_index - retrieves the node at a given index
+ * @head: pointer to the head of the list
+ * @index: index of the node to retrieve
+ *
+ * Return: pointer to the node at the specified index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index){
+    dlistint_t *temp = head;
+    unsigned int count = 0;
+
+    while (temp != NULL) {
+        if (count == index) {
+            return temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    return NULL;
+}
