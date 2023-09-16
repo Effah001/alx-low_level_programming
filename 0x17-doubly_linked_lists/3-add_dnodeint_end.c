fc9a2956ede41b3ce34ec8d2fe4f93dddb0c3bd3
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-16 08:42:52 +0000

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,8 +11,8 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *current = *head;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current = *head;
 
     if (new_node == NULL)
         return (NULL);
