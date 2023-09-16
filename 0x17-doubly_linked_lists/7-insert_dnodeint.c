25a7128f64e8688d2b0500d492e5eb7e6e820786
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-16 11:06:59 +0000

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,39 +10,48 @@
  * Return: the address of the new node, or NULL if it failed
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n) {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    if (idx == 0) {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-    } else {
-        dlistint_t *current = *h;
-        unsigned int i = 0;
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+	}
+	else
+	{
+		dlistint_t *current = *h;
+		unsigned int i = 0;
 
-        while (i < idx - 1 && current != NULL) {
-            current = current->next;
-            i++;
-        }
+		while (i < idx - 1 && current != NULL)
+		{
+			current = current->next;
+			i++;
+		}
 
-        if (current != NULL) {
-            new_node->prev = current;
-            new_node->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = new_node;
-            current->next = new_node;
-        } else {
-            free(new_node);
-            return NULL;
-        }
-    }
+		if (current != NULL)
+		{
+			new_node->prev = current;
+			new_node->next = current->next;
+			if (current->next != NULL)
+	
+			current->next->prev = new_node;
+			current->next = new_node;
+		}
+		else
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
 
-    return new_node;
+	return (new_node);
 }
