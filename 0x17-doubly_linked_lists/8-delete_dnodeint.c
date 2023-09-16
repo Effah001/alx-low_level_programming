772fa05ece13a7742b41a8cf9fe27e0160256190
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-16 11:20:03 +0000

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,33 +7,35 @@
  * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) {
-    dlistint_t *current = *head;
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
 
-    if (current == NULL)
-        return (-1);
+	if (current == NULL)
+		return (-1);
 
-    if (index == 0) {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    unsigned int i = 0;
-
-    while (current != NULL) {
-        if (i == index) {
-            current->prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = current->prev;
-            free(current);
-            return (1);
-        }
-        current = current->next;
-        i++;
-    }
-
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	unsigned int i = 0;
+	
+    while (current != NULL)
+	{
+		if (i == index)
+	{
+		current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			free(current);
+			return (1);
+	}
+		current = current->next;
+		i++;
+	}
     return (-1);
 }
