3ec1a354774c96defaeb0b8faafaa7c05e1fab4c
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 19:31:52 +0000

    Create _memset.c

---
+++ 0x18-dynamic_libraries/_memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memset -  a function that fills memory with a constant byte
+*@s : string to be filled
+*@b : character to be filled inside the memory
+*@n : number of memory space to be filled
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x = 0;
+
+	while (x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
