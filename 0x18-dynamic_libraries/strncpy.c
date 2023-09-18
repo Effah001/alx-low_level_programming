a6738d64be8ba19d4ac3edc5ccee76d5ebacdbef
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 19:24:42 +0000

    Create strncpy.c

---
+++ 0x18-dynamic_libraries/strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strncpy - copies a sting into another string
+*@dest : destination string to be copied to
+*@src : source string
+*@n : number of strings to be copied
+*Return: nothing
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+	{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+	dest[i] = src[i];
+	i++;
+	}
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+	return (dest);
+	}
