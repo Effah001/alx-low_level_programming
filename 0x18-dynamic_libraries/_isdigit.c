2309eef5a4076e14767cbd67295fffeaa984d604
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 19:22:55 +0000

    Create _isdigit.c

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit -check for a digit from 0 through 9
+ * @c: is the int to be checked
+ * Return: 1 if c is a digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n');
+}
