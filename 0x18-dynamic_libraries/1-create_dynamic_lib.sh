d01d009d4662e1895deaf32f5af282339aeac2fa
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 20:38:35 +0000

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c -fpic *.c
+gcc *.o -shared -o liball.so
+sudo mv liball.so /usr/lib
