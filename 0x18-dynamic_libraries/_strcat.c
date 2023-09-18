a0dd60b3da79cc7a15a1af58f01917e217721c9e
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 20:12:01 +0000

    Update _strcat.c

--- 0x18-dynamic_libraries/_strcat.c
+++ 0x18-dynamic_libraries/_strcat.c
@@ -1,1 +1,31 @@
+#include "main.h"
+
+/**
+* _strncat - concatenate two strings based on n
+*@dest : destination string to be concatenated to
+*@src : source string
+*Return: nothing
+*/
+
 char *_strcat(char *dest, char *src);
+
+{
+
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+	dest[i] = src[j];
+	j++;
+	i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+	}
