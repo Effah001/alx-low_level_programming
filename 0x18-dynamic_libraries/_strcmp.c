0b78753fddb000e91ce39c22839f4fe8a2714f6e
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 19:30:27 +0000

    Create _strcmp.c

---
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcmp - compare to strings
+*@s1: pointer to a character
+*@s2: pointer to a character
+* Return: nothing
+*/
+
+	int _strcmp(char *s1, char *s2)
+	{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+	i++;
+	}
+	if (s1[i] == s2[i])
+	{
+	return (0);
+	}
+	else if (s1[i] > s2[i])
+	{
+		return (1);
+	}
+	else
+		return (-1);
+}
