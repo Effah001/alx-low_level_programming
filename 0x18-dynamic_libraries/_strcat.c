5ac4a0aa22166a2c9ff3b003ff4b180367033273
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-18 20:13:59 +0000

    Update _strcat.c

--- 0x18-dynamic_libraries/_strcat.c
+++ 0x18-dynamic_libraries/_strcat.c
@@ -10,21 +10,4 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int i = 0;
-	int j = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	while (src[j] != '\0' && j < n)
-	{
-	dest[i] = src[j];
-	j++;
-	i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-	}
+}
