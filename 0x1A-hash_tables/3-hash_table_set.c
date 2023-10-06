c7622322922a0cf6902417a8e6b3b68dcc684181
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-06 17:22:33 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(current->value);
 			current->value = strdup(value);
 			if (!current->value)
-					return (0);
+				return (0);
 			return (1);
 		}
 			current = current->next;
