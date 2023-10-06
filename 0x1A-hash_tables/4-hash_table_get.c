ab3644caa061fc309d3e1fadb30223781e9df805
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-06 13:08:50 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,7 +7,7 @@
  * Return: The value
  */
 
-char *hash_table_get (const hash_table_t *ht, const char *key)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
 	unsigned long int index;
