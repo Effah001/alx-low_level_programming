efff7f34743e2bac12b6eba5820262c18354e192
Kali <effahfrancis210@gmail.com>
2023-10-06 10:51:37 -0400

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,35 +1,28 @@
 #include "hash_tables.h"
+#include <stdio.h>
 
 /**
- * hash_table_print - prints the table
- * @ht: hash table
- *
- * Return: nothing
+ * hash_table_print - prints the key-value pairs of a hash table
+ * @ht: the hash table
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_node_t *current;
-	int flag = 1;
+    if (!ht)
+        return;
 
-	if (!ht)
-	{
-		return;
-	}
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		current = ht->array[i];
-		while (current != NULL)
-		{
-			if (flag)
-				flag = 0;
-			else
-				printf(", ");
-			printf("'%s': '%s'", current->key, current->value);
-			current = current->next;
-		}
-	}
-		printf("}\n");
+    printf("{");
+    for (i = 0; i < ht->size; ++i)
+    {
+        hash_node_t *current = ht->array[i];
+        while (current)
+        {
+            printf("'%s': '%s'", current->key, current->value);
+            if (current->next)
+                printf(", ");
+            current = current->next;
+        }
+    }
+    printf("}\n");
 }
+
---
+++ 0x1A-hash_tables/5-hash_table_print1.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the table
+ * @ht: hash table
+ *
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *current;
+	int flag = 1;
+
+	if (!ht)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (flag)
+				flag = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+		}
+	}
+		printf("}\n");
+}
