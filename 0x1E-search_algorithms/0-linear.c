3ae5d7a03ff8cfe24e61ae511904248ce73a7a9a
Droid <44108804+Effah001@users.noreply.github.com>
2024-02-07 14:11:04 +0000

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,15 +11,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-		size_t i;
+		size_t i = 0;
 
-		if value == NULL
+		if (array == NULL)
 			return (-1);
 
 		for (i = 0; i < size; i++)
 		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
 		}
-			return(-1)
+			return(-1);
 }
